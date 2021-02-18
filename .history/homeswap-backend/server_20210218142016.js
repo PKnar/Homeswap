66cdb110cd0dc1671bb048718c2f0a4c62cf37a1
Poghosyan,Knarik K <k.poghosyan@student.fontys.nl>
2021-02-18 22:34:14 +0100

    package.json config

--- .history/homeswap-backend/server_20210218142016.js
+++ .history/homeswap-backend/server_20210218142016.js
@@ -9,12 +9,4 @@ app.get("/", (req, res) => {
   res.json({ message: "Backend is working" });
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "homeswap/build")));
-
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "homeswap/build", "index.html"));
-  });
-}
-
 app.listen(PORT, () => console.log(`Connected to port ${PORT}`));
--- .history/homeswap-backend/server_20210218221650.js
+++ .history/homeswap-backend/server_20210218221650.js
@@ -10,10 +10,10 @@ app.get("/", (req, res) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "homeswap/build")));
+  app.use(express.static(path.join(__dirname, "frontend/build")));
 
   app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "homeswap/build", "index.html"));
+    res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
   });
 }
 
